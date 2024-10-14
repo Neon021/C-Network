2dde6a81b048d12c9f5f79997bf6e82ca4a28927
Adrian Yigit <bilalyigit594@gmail.com>
2024-10-14 17:33:50 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Listen();
+    }
+
+    public static async void Listen()
+    {
+        IPEndPoint iPEndPoint = new(IPAddress.Any, 6379);
+        using Socket socket = new(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        {
+            await socket.ConnectAsync(iPEndPoint);
+            byte[] pong = Encoding.UTF8.GetBytes("+PONG\r\n");
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                _ = socket.ReceiveAsync(buffer, SocketFlags.None);
+
+                await socket.SendAsync(pong);
+            }
+        }
+    }
+}
